a07da7d7f34cd96a6ec469318a8ed9d263e09bdc
SNEHASIS DUTTA <162689269+SNEHASIS-CODEHUB04@users.noreply.github.com>
2024-08-13 09:39:05 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,6 @@ import React from "react";
 function Logo({ width = "100px", color }) {
   return (
     <>
-      {/* <img src="/logo.svg" alt="alt" /> */}
       <div style={{ color: color }}>Logo</div>
     </>
   );
