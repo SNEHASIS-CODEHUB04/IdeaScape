e98cdf9a98b714879edb1d76cd51a6d55ce8dc7e
Razak Attar <165865038+razak571@users.noreply.github.com>
2024-08-13 14:14:51 +0530

    fix : after post creation it should redirect to post component but not sign up

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,12 +1,73 @@
-import React from "react";
-import { Signup as SignupComponent } from "../components";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Button, Container } from "../components";
+import { useSelector } from "react-redux";
+import appwriteService from "../appwrite/config";
+import parse from "html-react-parser";
 
-function Signup() {
-  return (
+function Post() {
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
+  const isAuthor = post && userData ? post.userId === userData.$id : false;
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) setPost(post);
+        else navigate("/");
+      });
+    } else navigate("/");
+  }, [slug, navigate]);
+
+  const deletePost = () => {
+    appwriteService.deletePost(post.$id).then((status) => {
+      if (status) {
+        appwriteService.deleteFile(post.featuredImage);
+        navigate("/");
+      }
+    });
+  };
+
+  return post ? (
     <div className="py-8">
-      <SignupComponent />
+      <Container>
+        <div className="w-full flex justify-center mb-4 relative  rounded-xl p-2">
+          <img
+            src={appwriteService.getFilePreview(post.featuredImage)}
+            alt={post.title}
+            className="rounded-xl "
+          />
+
+          {isAuthor && (
+            <div className="absolute right-6 top-6">
+              <Link to={`/edit-post/${post.$id}`}>
+                <Button bgColor="bg-green-500" className="mr-3">
+                  Edit
+                </Button>
+              </Link>
+              <Button
+                bgColor="bg-red-500"
+                className="mr-3"
+                onClick={deletePost}
+              >
+                Delete
+              </Button>
+              <Link to="/">
+                <Button bgColor="bg-blue-500">Home</Button>
+              </Link>
+            </div>
+          )}
+        </div>
+
+        <div className="w-full mb-6 text-white">
+          <h1 className="text-2xl font-bold">{post.title} </h1>
+        </div>
+        <div className="browser-css">{parse(post.content)} </div>
+      </Container>
     </div>
-  );
+  ) : null;
 }
 
-export default Signup;
+export default Post;
